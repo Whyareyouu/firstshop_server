727aae893537a9fd6f49147050a4f7df6799b040
whyareyou <bionixxxd5@gmail.com>
2023-07-08 13:53:01 +0300

    add auth, add roles/auth guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,12 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {AddRoleDto} from "./dto/add-role.dto";
 
 @ApiTags('users')
 @Controller('users')
@@ -20,8 +24,19 @@ export class UsersController {
 
     @ApiOperation({summary: 'Get all users'})
     @ApiResponse({status: 200, type: [User]})
+    @Roles('admin')
+    @UseGuards(RolesGuard)
     @Get()
     getAll(){
         return this.usersService.getAllUsers()
+    }
+
+    @ApiOperation({summary: 'Give user role(only for admin)'})
+    @ApiResponse({status: 200})
+    @Roles('admin')
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto){
+        return this.usersService.addRole(dto)
     }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,22 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import {forwardRef, Module} from '@nestjs/common';
+import {UsersController} from './users.controller';
+import {UsersService} from './users.service';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {User} from "./users.model";
 import {Role} from "../roles/roles.model";
 import {UserRoles} from "../roles/user-role.model";
+import {AuthModule} from "../auth/auth.module";
 import {RolesModule} from "../roles/roles.module";
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
-  imports: [
-      SequelizeModule.forFeature([User, Role, UserRoles]),
-      RolesModule
-  ]
+    controllers: [UsersController],
+    providers: [UsersService],
+    imports: [
+        SequelizeModule.forFeature([User, Role, UserRoles]),
+        RolesModule,
+        forwardRef(() => AuthModule),
+    ],
+    exports: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule {
+}
